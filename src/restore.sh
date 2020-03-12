d908130340ae2917300d34ca74c12ed040823f49
Josip Medved <jmedved@jmedved.com>
2020-03-11 18:54:10 -0700

    Refactoring variables

--- src/restore.sh
+++ src/restore.sh
@@ -4,9 +4,10 @@ CHANGED=0
 
 for VER in "2.1" "3.0"; do
     FILE_TEMPLATE=/usr/lib/surface-go-wifi/template/hw$VER/board.bin
+    FILE_DRIVER=/lib/firmware/ath10k/QCA6174/hw$VER/board.bin
+    FILE_BACKUP=/usr/lib/surface-go-wifi/backup/hw$VER/board.bin
+
     if [[ -f $FILE_TEMPLATE ]]; then
-        FILE_DRIVER=/lib/firmware/ath10k/QCA6174/hw$VER/board.bin
-        FILE_BACKUP=/usr/lib/surface-go-wifi/backup/hw$VER/board.bin
         cmp --silent $FILE_TEMPLATE $FILE_DRIVER
         if [[ $? -ne 0 ]]; then
             cp $FILE_DRIVER $FILE_BACKUP
