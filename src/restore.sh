6d316a5f1c33d6ce20b2828284364d953b6156ef
Josip Medved <jmedved@jmedved.com>
2020-03-11 20:16:26 -0700

    Generalized file checks a bit

--- src/restore.sh
+++ src/restore.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 
-CHANGED=0
+CHANGED_ANY=0
 
-for VER in "2.1" "3.0"; do
-    FILE_TEMPLATE=/usr/lib/surface-go-wifi/template/hw$VER/board.bin
-    FILE_DRIVER=/lib/firmware/ath10k/QCA6174/hw$VER/board.bin
-    FILE_BACKUP=/usr/lib/surface-go-wifi/backup/hw$VER/board.bin
+for HW in "hw2.1" "hw3.0"; do
+    CHANGED_HW=0
 
-    if [[ -f $FILE_TEMPLATE ]]; then
-        cmp --silent $FILE_TEMPLATE $FILE_DRIVER
-        if [[ $? -ne 0 ]]; then
-            cp $FILE_DRIVER $FILE_BACKUP
-            cp $FILE_TEMPLATE $FILE_DRIVER
-            echo "Restored Surface Go WiFi (hw$VER)"
-            CHANGED=1
+    for FILE in `ls /usr/lib/surface-go-wifi/template/$HW/`; do
+        FILE_TEMPLATE=/usr/lib/surface-go-wifi/template/$HW/$FILE
+        FILE_DRIVER=/lib/firmware/ath10k/QCA6174/$HW/$FILE
+        FILE_BACKUP=/usr/lib/surface-go-wifi/backup/$HW/$FILE
+
+        if [[ -f $FILE_TEMPLATE ]]; then
+            cmp --silent $FILE_TEMPLATE $FILE_DRIVER
+            if [[ $? -ne 0 ]]; then
+                cp $FILE_DRIVER $FILE_BACKUP
+                cp $FILE_TEMPLATE $FILE_DRIVER
+                CHANGED_HW=1
+                CHANGED_ANY=1
+            fi
         fi
+    done
+
+    if [[ $CHANGED_HW -ne 0 ]]; then
+        echo "Restored Surface Go WiFi ($HW)"
     fi
 done
 
-if [[ $CHANGED -ne 0 ]]; then
+if [[ $CHANGED_ANY -ne 0 ]]; then
     echo "Reboot for changes to take effect."
 fi
 
